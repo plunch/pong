8d9467641894b925678a6c72afaa7e8d959b96c9
Per Lundh <plundh@perlundh.com>
2016-02-07 17:43:38 +0100

    Missed adding these

---
+++ menus/winmenu.c
@@ -0,0 +1,59 @@
+#include "winmenu.h"
+
+#include "../global.h"
+
+struct winmenu_state {
+	int winner;
+};
+
+static void winmenu_paint(void* userdata, SDL_Renderer* re)
+{
+	struct winmenu_state* s = userdata;
+
+	int w, h;
+	SDL_GetRendererOutputSize(re, &w, &h);
+
+	SDL_Rect r;
+	r.x = 0;
+	r.y = 0;
+	r.w = w / 2;
+	r.h = h;
+
+	switch (s->winner) {
+		default:
+			r.w = w;
+			break;
+		case 1:
+			break;
+		case 2:
+			r.x = w / 2;
+			break;
+	}
+
+	SDL_SetRenderDrawColor(re, 255, 255, 255, SDL_ALPHA_OPAQUE);
+	SDL_RenderFillRect(re, &r);
+}
+
+static enum menu_result winmenu_action(void* userdata, enum action action)
+{
+	UNUSED(userdata);
+	
+	if (action != ACT_NONE) 
+		return MNU_FORWARD;
+	else
+		return MNU_NONE;
+}
+
+int create_winmenu(struct menu* m, int winner)
+{
+	struct winmenu_state* state = malloc(sizeof(struct winmenu_state));
+	if (state == NULL)
+		return 0;
+
+	state->winner = winner;
+
+	m->userdata = state;
+	m->paint = &winmenu_paint;
+	m->action = &winmenu_action;
+	return 1;
+}
---
+++ menus/winmenu.h
@@ -0,0 +1,8 @@
+#ifndef WIN_H
+#define WIN_H
+
+#include "../menuimpl.h"
+
+int create_winmenu(struct menu*, int winner);
+
+#endif /* WIN_H */
