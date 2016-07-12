776db61c60a8e3ff50ba479705197b4037bac6f5
Per Lundh <plundh@perlundh.com>
2016-07-12 23:07:15 +0200

    Quiet warnings

--- text.c
+++ text.c
@@ -6,13 +6,16 @@
 static int print_char(SDL_Renderer* rend, struct textinfo* t,
                        const char* c, SDL_Rect* bounds, enum text_alignment a)
 {
+	// TODO: Render text right-aligned
+	(void)(a);
+
 	if (*c > 31 && *c < 127) {
 		SDL_Rect* src = &(t->chars[*c-31]);
 
 		int bw = bounds->w;
 
 		double ratio = src->w / (double)src->h;
-		int w = rint(bounds->h * ratio);
+		int w = (int)(rint(bounds->h * ratio));
 		bounds->w = w;
 
 		SDL_RenderCopy(rend, t->b, src, bounds);
