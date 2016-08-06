561558ccb6f0211ef7cfa1a162940f65f00cfe58
Per Lundh <plundh@perlundh.com>
2016-08-06 17:43:23 +0200

    Add simple joystick input

---
+++ input/joystick.c
@@ -0,0 +1,91 @@
+#include "joystick.h"
+#include <assert.h>
+#include <stdlib.h> /* abs */
+
+static void ball(struct joystick_mapping* m, struct input* in, SDL_JoyBallEvent* e)
+{
+	for(size_t i = 0; i < m->balen; ++i) {
+		if (m->balls[i].joystick == e->which
+		 && m->balls[i].ball == e->ball) {
+			real input;
+			if (m->balls[i].horizontal)
+				input = (real)e->xrel;
+			else
+				input = (real)e->yrel;
+
+			in->input[m->balls[i].action] = input;
+		}
+	}
+}
+
+static void axis(struct joystick_mapping* m, struct input* in, SDL_JoyAxisEvent* e)
+{
+	for(size_t i = 0; i < m->alen; ++i) {
+		if (m->axes[i].joystick == e->which
+		 && m->axes[i].axis == e->axis) {
+			real input = 0;
+			if (m->axes[i].deadzone > abs(e->value))
+				input = (real)e->value;
+			in->input[m->axes[i].action] = input;
+		}
+	}
+}
+
+static void button(struct joystick_mapping* m, struct input* in, SDL_JoyButtonEvent* e)
+{
+	for(size_t i = 0; i < m->blen; ++i) {
+		if (m->buttons[i].joystick == e->which
+		 && m->buttons[i].button == e->button) {
+			real input = e->state == SDL_PRESSED ? 1 : 0;
+			in->input[m->buttons[i].action] = input;
+		}
+	}
+}
+
+static void hat(struct joystick_mapping* m, struct input* in, SDL_JoyHatEvent* e)
+{
+	for(size_t i = 0; i < m->hlen; ++i) {
+		if (m->hats[i].joystick == e->which
+		 && m->hats[i].hat == e->hat) {
+			real input = 0;
+			if (m->hats[i].ppos == e->value)
+				input = 1;
+			else if (m->hats[i].npos == e->value)
+				input = -1;
+			in->input[m->hats[i].action] = input;
+		}
+	}
+}
+
+
+
+static void event(void* data, struct input* in, SDL_Event* e)
+{
+	struct joystick_mapping* m = data;
+
+	switch(e->type) {
+		case SDL_JOYBALLMOTION:
+			ball(m, in, &e->jball);
+			return;
+		case SDL_JOYAXISMOTION:
+			axis(m, in, &e->jaxis);
+			return;
+		case SDL_JOYBUTTONUP:
+		case SDL_JOYBUTTONDOWN:
+			button(m, in, &e->jbutton);
+			return;
+		case SDL_JOYHATMOTION:
+			hat(m, in, &e->jhat);
+			return;
+		default:
+			return;
+	}
+}
+
+void jsinput_make_inputsource(struct inputsource_sdl* target, struct joystick_mapping* map)
+{
+	assert(target != NULL);
+	assert(map != NULL);
+	target->data = map;
+	target->apply = &event;
+}
---
+++ input/joystick.h
@@ -0,0 +1,47 @@
+#ifndef INPUT_JOYSTICK_H
+#define INPUT_JOYSTICK_H
+
+#include "../input.h"
+#include "../inputsource_sdl.h"
+
+#include <SDL2/SDL.h>
+
+struct joyaxis_mapping {
+	Uint8 joystick;
+	Uint8 axis;
+	Sint16 deadzone;
+	enum game_action action;
+};
+
+struct joybutton_mapping {
+	Uint8 joystick;
+	Uint8 button;
+	enum game_action action;
+};
+
+struct joyhat_mapping {
+	Uint8 joystick;
+	Uint8 hat;
+	Uint8 ppos, npos;
+	enum game_action action;
+};
+
+struct joyball_mapping {
+	Uint8 joystick;
+	Uint8 ball;
+	unsigned int horizontal : 1;
+	enum game_action action;
+};
+
+struct joystick_mapping {
+	struct joyaxis_mapping* axes;
+	struct joybutton_mapping* buttons;
+	struct joyhat_mapping* hats;
+	struct joyball_mapping* balls;
+	size_t alen, blen, balen, hlen;
+};
+
+
+void jsinput_make_inputsource(struct inputsource_sdl*, struct joystick_mapping*);
+
+#endif /* INPUT_JOYSTICK_H */
